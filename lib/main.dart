887d19b34ba2bd5dfb6a9f02580c6d1d4b795f88
rakib hussain <rakibhussain8812@gmail.com>
2025-12-30 20:46:23 +0530

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,29 @@ class NewApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.red),
-        body: Text("sakib"),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: Text("Rakib First Application"),
+          actions: [Icon(Icons.home), Icon(Icons.heart_broken)],
+          leading: CircleAvatar(child: Text("R")),
+        ),
+        body: Text("Rakib"),
       ),
     );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
